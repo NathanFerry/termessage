2848346fb6f6c6386526901251828598565ffd0b
Nathan FERRY <nathan.ferry@live.fr>
2026-02-04 15:09:54 +0100

    nslookup equivalent

--- src/main.c
+++ src/main.c
@@ -5,12 +5,48 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <stdio.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        fprintf(stderr, "To many arguments");
+        return 1;
+    }
+
+    int status;
     struct addrinfo hints = {0}, *servinfo;
+    char ipstr[INET6_ADDRSTRLEN];
 
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+
+    if ((status = getaddrinfo(argv[1], "3490", &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    }
+
+    printf("IP addresses for %s\n\n", argv[1]);
+
+    for (struct addrinfo *p = servinfo; p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf(" %s: %s\n", ipver, ipstr);
+    }
+
+    freeaddrinfo(servinfo);
 }
